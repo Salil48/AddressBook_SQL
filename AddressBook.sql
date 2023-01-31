b0c8967ad1a1e953e4dd0d03998cd1d08861650b
“Salil48” <salilchharia@gmail.com>
2023-01-31 20:53:38 +0530

    Insert Values

--- AddressBook.sql
+++ AddressBook.sql
@@ -12,3 +12,12 @@ zip BigInt,
 PhoneNumber BigInt,
 Email varchar(200)
 )
+
+
+------ UC 3: Insert Values to Table ------
+Insert into Address_Book_Table(FirstName,SecondName,Address,City,State,zip,PhoneNumber,Email) 
+values('Harsh','Vardhan','645 Catherine Street','Bangalore','Karnataka',243001,9842905050,'harsh@gmail.com'),
+('Ramesh','Kumar','836 Heritage Rd','Chennai','Tamil Nadu',134002,98402000,'Ramesh@gmail.com'),
+('Anita','Yadav','19 Augusta Avenue','Lucknow','Uttar Pradesh',113201,87210505053,'anita@gmail.com');
+
+select * from Address_Book_Table
